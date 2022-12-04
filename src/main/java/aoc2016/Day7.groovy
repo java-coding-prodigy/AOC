07c3a76aae2dcd70b0c0e1eb737e16ab6515c77a
java-coding-prodigy <java-coding-prodigy@github.com>
2022-12-04 17:46:20 +0530

    2016.7

--- src/main/java/aoc2016/Day7.groovy
+++ src/main/java/aoc2016/Day7.groovy
@@ -11,12 +11,31 @@ class Day7 extends Day {
 
     @Override
     void run(List<String> input) {
-        def part1 = input.count {
-            def (a, b, c) = it.split('[\\[\\]]')
-            println("$a $b $c")
-            (abba(a) || abba(c)) && !abba(b)
+        def part1 = 0
+        def part2 = 0
+        input.each {
+            def ip = it.split('[\\[\\]]')
+            def res = null
+            Set<String> aba = new HashSet<>()
+            Set<String> bab = new HashSet<>()
+            ip.eachWithIndex { String text, int i ->
+                if (i % 2 == 0) {
+                    if (abba(text))
+                        res = res == null ? true : res
+                    aba.addAll(ssl(text))
+                } else {
+                    if (abba(text))
+                        res = false
+                    bab.addAll(ssl(text))
+                }
+            }
+            if (res)
+                part1++
+            if (aba.any { s -> (bab.contains(s.reverse())) })
+                part2++
         }
         println "Part 1: $part1"
+        println "Part 2: $part2"
     }
 
     private static boolean abba(String str) {
@@ -25,5 +44,14 @@ class Day7 extends Day {
                 return true
         }
         return false
+    }
+
+    private static Set<String> ssl(String str) {
+        def abas = new HashSet<String>()
+        for (int i = 0; i + 3 <= str.length(); i++) {
+            if (str[i] == str[i + 2] && str[i] != str[i + 1])
+                abas.add("" + str[i] + str[i + 1])
+        }
+        return abas
     }
 }
